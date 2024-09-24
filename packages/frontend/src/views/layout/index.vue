9abc8d39fcde3a0ad2e48641e9a9f3061b00f91b
gengwenhao789 <gengwenhao789@zto.com>
2024-09-24 11:19:03 +0800

    feat: fronted-加入layout布局

---
+++ packages/frontend/src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="layout-container">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad culpa cumque debitis, dignissimos dolor dolores ducimus
+    eaque, error hic maiores neque nihil repellat saepe, sint vitae. At inventore provident rem?
+    <router-view />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
