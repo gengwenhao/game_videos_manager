383dc19b5f7cd7d0d0a389e170dcb840be152e19
gengwenhao <gengwenhao97@126.com>
2024-09-24 22:34:16 +0800

    feat: frontend-引入进度条

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -1,6 +1,14 @@
+import 'nprogress/nprogress.css'
+
 import { createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
 
 import routes from './routes'
+
+// 修改进度条插件的配置
+NProgress.configure({
+  showSpinner: false
+})
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +18,13 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   // @ts-ignore
   to.meta.title && (document.title = to.meta.title)
+  NProgress.start()
 
   next()
+})
+
+router.afterEach((to) => {
+  NProgress.done()
 })
 
 export default router
