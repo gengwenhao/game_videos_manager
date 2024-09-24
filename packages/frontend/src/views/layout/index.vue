d6eb4628041122bb6a686fae1ff6e08813f6a2de
gengwenhao <gengwenhao97@126.com>
2024-09-24 22:14:16 +0800

    feat: frontend-引入了 ElementPlus

--- packages/frontend/src/views/layout/index.vue
+++ packages/frontend/src/views/layout/index.vue
@@ -3,13 +3,29 @@
 </script>
 
 <template>
-  <div class="layout-container">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad culpa cumque debitis, dignissimos dolor dolores ducimus
-    eaque, error hic maiores neque nihil repellat saepe, sint vitae. At inventore provident rem?
-    <router-view />
-  </div>
+  <el-container class="common-layout">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <style scoped>
+.el-container {
+  min-height: 100vh;
+}
 
+.el-aside {
+  background-color: red;
+}
+
+.el-header {
+  background-color: lightblue;
+}
+
+.el-main {
+  background-color: lightgoldenrodyellow;
+}
 </style>
